6897e1732f39f7840e2d846b0f8169f166474281
tko81 <104875084+tko81@users.noreply.github.com>
2024-05-01 22:01:25 +0800

    Update 10_17.cpp

--- C++Primer/Chapter10/10_17.cpp
+++ C++Primer/Chapter10/10_17.cpp
@@ -30,9 +30,9 @@ void biggies(vector<string> &svec, vector<string>::size_type sz){
                 [](const string &a, const string &b) {return a.size() < b.size();});
     //true放在左边 false放在右边 算法返回一个迭代器，指向最后一个使谓词为true的元素之后的位置
     auto iter = partition(svec.begin(), svec.end(),
-                        [sz](const string &s) {return s.size() < sz;}); 
+                        [sz](const string &s) {return s.size() >= sz;}); 
                         
-    auto count = svec.end() - iter;
+    auto count = iter - svec.begin();
     cout << count << " " << make_plural(count, "word", "s") << " of length "  << sz << " or longer : ";
     for_each(iter, svec.end(), [](const string &s){cout << s << " ";});
     cout << endl;
