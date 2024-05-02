88239247a0045fe647b1445647a01789f811ce39
tko81 <104875084+tko81@users.noreply.github.com>
2024-05-02 12:09:37 +0800

    Create 10_28.cpp

---
+++ C++Primer/Chapter10/10_28.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9};
+    list<int> lis;
+    //unique_copy(vec.begin(), vec.end(), back_inserter(lis));
+    //unique_copy(vec.begin(), vec.end(), front_inserter(lis));
+    unique_copy(vec.begin(), vec.end(), inserter(lis, lis.begin()));
+
+    for(const auto &i : lis){
+        cout << i << " ";
+    }
+    return 0;
+}
