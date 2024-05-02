fb16a571111bcb670a978b542925e2b1251031c8
tko81 <104875084+tko81@users.noreply.github.com>
2024-05-02 11:32:42 +0800

    Create 10_25.cpp

---
+++ C++Primer/Chapter10/10_25.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using namespace std::placeholders;
+
+void elimDups (vector<string> &svec);
+bool isbigger(const string &s, vector<string>::size_type sz);
+void biggies(vector<string> &svec, vector<string>::size_type sz);
+string make_plural(int count, const string &a, const string &b);
+
+int main() {
+    vector<string> svec = {"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle"};
+    biggies(svec, 4);
+    return 0;
+}
+
+void elimDups (vector<string> &svec){
+    sort(svec.begin(), svec.end());
+    auto end_unique = unique(svec.begin(), svec.end());
+    svec.erase(end_unique, svec.end());
+}
+string make_plural(int count, const string &a, const string &b){
+    if(count > 1) return a + b;
+    else return a;
+}
+bool isbigger(const string &s, vector<string>::size_type sz){
+    return s.size() >= sz;
+}
+void biggies(vector<string> &svec, vector<string>::size_type sz){
+    elimDups(svec);
+    stable_sort(svec.begin(), svec.end(),
+                [](const string &a, const string &b) {return a.size() < b.size();});
+    //true放在左边 false放在右边 算法返回一个迭代器，指向最后一个使谓词为true的元素之后的位置
+    auto iter = partition(svec.begin(), svec.end(),
+                          bind(isbigger, _1, sz));
+
+    auto count = iter - svec.begin();
+    cout << count << " " << make_plural(count, "word", "s") << " of length "  << sz << " or longer : ";
+    for_each(svec.begin(), iter, [](const string &s){cout << s << " ";});
+    cout << endl;
+}
