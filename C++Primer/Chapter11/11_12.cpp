3b958339ed0e75c02753d8fdefd217e53b142d18
tko81 <104875084+tko81@users.noreply.github.com>
2024-05-02 21:32:00 +0800

    Update 11_12.cpp

--- C++Primer/Chapter11/11_12.cpp
+++ C++Primer/Chapter11/11_12.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <utility>
 
+using namespace std;
+
+int main() {
+    vector<pair<string, int>> pvec;
+    string word;
+    int no;
+    while(cin >> word && cin >> no){
+        pvec.push_back({word, no});
+//      pvec.push_back(pair<string, int> (word, no));
+//      pvec.push_back(make_pair(word, no));
+    }
+    for(const auto &i : pvec){
+        cout << i.first << " " << i.second << endl;
+    }
+    return 0;
+}
