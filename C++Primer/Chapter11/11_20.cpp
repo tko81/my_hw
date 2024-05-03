3aec4ad88fa31fe1b78e1f2699ab286af5dbe2f1
tko81 <104875084+tko81@users.noreply.github.com>
2024-05-03 09:20:33 +0800

    Create 11_20.cpp

---
+++ C++Primer/Chapter11/11_20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<string, size_t> word_count;
+    string word;
+    while(cin >> word){
+        auto it = word_count.emplace(word, 1);
+        //auto it = word_count.insert({word, 1});
+        if(!it.second){
+            ++word_count[word];
+        }
+    }
+    for(const auto &s : word_count){
+        cout << s.first << " occurs " << s.second << (s.second > 1 ? "times" : "time") << endl;
+    }
+    return 0;
+}
